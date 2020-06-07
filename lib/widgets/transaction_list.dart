07b821ffff73e489bdf05da95084c8c68ffc494b
Sarahmdawood <sarahmajeed368@gmail.com>
2020-06-07 16:22:03 +0500

    Adding list view widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,38 +10,44 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: transactions.map((tx) {
-      return Card(
-          child: Row(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(border: Border.all(color: Colors.purple)),
-            child: Text(
-              '\$${tx.amount}',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.purple),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                tx.title.toString(),
-                style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                DateFormat.yMMMMEEEEd().format(tx.date),
-                style: TextStyle(color: Colors.grey),
-              )
-            ],
-          )
-        ],
-      ));
-    }).toList());
+    return Container(
+        height: 300,
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Card(
+                child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  padding: EdgeInsets.all(10),
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.purple)),
+                  child: Text(
+                    '\$${transactions[index].amount}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title.toString(),
+                      style:
+                          TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat.yMMMMEEEEd().format(transactions[index].date),
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                )
+              ],
+            ));
+          },
+          itemCount: transactions.length,
+        ));
   }
 }
