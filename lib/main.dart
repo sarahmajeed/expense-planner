6ec8d84486b6c9e88051080a34404c43dc4c6946
Sarahmdawood <sarahmajeed368@gmail.com>
2020-06-14 15:57:28 +0500

    Final touches

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Personal Expenses',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.purple,
           errorColor: Colors.red,
